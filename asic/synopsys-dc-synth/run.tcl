d8d682bc9371a094a331787d8dab0162bd99421b
cb535 <cb535@cornell.edu>
2023-02-02 22:03:06 -0500

    ready for 2023

--- asic/synopsys-dc-synth/run.tcl
+++ asic/synopsys-dc-synth/run.tcl
@@ -1,8 +1,8 @@
 
 set_app_var target_library "$env(ECE5745_STDCELLS)/stdcells.db"
 set_app_var link_library   "* $env(ECE5745_STDCELLS)/stdcells.db"
-analyze -format sverilog ../../sim/build/RegIncr4stageRTL__pickled.v
-elaborate RegIncr4stageRTL
+analyze -format sverilog ../../sim/build/RegIncrNstage__p_nstages_4__pickled.v
+elaborate RegIncrNstage__p_nstages_4
 check_design
 create_clock clk -name ideal_clock1 -period 1
 compile
--- asic/synopsys-vcs-bagl-sim/run-1ns.sh
+++ asic/synopsys-vcs-bagl-sim/run-1ns.sh
@@ -1,12 +1,14 @@
+
 vcs -full64 -sverilog +lint=all -xprop=tmerge -override_timescale=1ns/1ps \
+  +incdir+../../sim/build \
+  +vcs+dumpvars+vcs-bagl-sim.vcd \
+  -top RegIncrNstage__p_nstages_4_tb \
   +define+CYCLE_TIME=1.0 \
   +define+VTB_INPUT_DELAY=0.1 \
   +define+VTB_OUTPUT_ASSERT_DELAY=0.99 \
   +neg_tchk +sdfverbose \
-  -sdf max:RegIncr4stageRTL_tb.DUT:../cadence-innovus-pnr/post-pnr.sdf \
-  +incdir+../../sim/build \
-  +vcs+dumpvars+vcs-bagl-sim.vcd \
-  -top RegIncr4stageRTL_tb \
+  -sdf max:RegIncrNstage__p_nstages_4_tb.DUT:../cadence-innovus-pnr/post-pnr.sdf \
   ../cadence-innovus-pnr/post-pnr.v \
   $ECE5745_STDCELLS/stdcells.v \
-  ../../sim/build/RegIncr4stageRTL_test_4stage_random_tb.v
+  ../../sim/build/RegIncrNstage__p_nstages_4_test_random_4_tb.v
+
